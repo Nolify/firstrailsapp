6b2aca1c7f9d10d25173197c4ef99ef771480872
Stephen Leguillon <stephen@nolify.com>
2012-12-10 11:45:43 +0100

    authentification

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Firstrailsapp::Application.routes.draw do
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  
+  get "sign_up" => "users#new", :as => "sign_up"
+#  root :to => "users#new"
+  
+  resources :users
+  resources :sessions
+  
   get "welcome/index"
-
+  root :to => "welcome#index"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +60,7 @@ Firstrailsapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  #  root :to => 'welcome#index'  
 
   # See how all your routes lay out with "rake routes"
 
